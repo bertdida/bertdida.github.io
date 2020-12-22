d366f72b67c6810ec863dcac18affe9e009a1a65
bertdida <38489033+bertdida@users.noreply.github.com>
2020-12-22 22:36:20 +0800

    Add support for external link

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,10 +2,22 @@ import { Link as RouterLink } from "react-router-dom";
 
 import "./Link.scss";
 
-export function Link({ children, ...rest }) {
+export function Link({ children, isExternal = false, ...rest }) {
+  if (isExternal) {
+    return <ExternalLink children={children} {...rest} />;
+  }
+
   return (
     <RouterLink className="link" {...rest}>
       <span className="link__children">{children}</span>
     </RouterLink>
+  );
+}
+
+function ExternalLink({ children, to, ...rest }) {
+  return (
+    <a className="link" href={to} {...rest}>
+      <span className="link__children">{children}</span>
+    </a>
   );
 }
