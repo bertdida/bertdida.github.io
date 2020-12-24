901121d7e1399cdff71d6fcc5044ab7994ff25d0
bertdida <38489033+bertdida@users.noreply.github.com>
2020-12-24 15:02:22 +0800

    Update for contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet";
 
+import { Link } from "../../components/Link";
 import "./Contact.scss";
 
 export function Contact() {
@@ -9,7 +10,27 @@ export function Contact() {
         <title>bertdida | contact</title>
       </Helmet>
 
-      <p>coming soon</p>
+      <section className="contact">
+        <h2 className="contact__title">Let's work together.</h2>
+
+        <p className="contact__text">
+          I'm available for interesting freelance work or projects. Big or
+          small. The best way to contact me is to shoot me an email at&nbsp;
+          <Link to="mailto:bertdida@gmail.com" isExternal>
+            bertdida@gmail.com
+          </Link>
+          &nbsp;or DM me on&nbsp;
+          <Link
+            to="https://www.linkedin.com/in/bertdida"
+            target="_blank"
+            rel="noreferrer"
+            isExternal
+          >
+            LinkedIn
+          </Link>
+          .
+        </p>
+      </section>
     </>
   );
 }
