1addfa06e8090ed76ec6f2fb58a49e28e4d9bcf7
bertdida <38489033+bertdida@users.noreply.github.com>
2020-12-29 20:41:48 +0800

    Render 404 page

--- src/Routes.js
+++ src/Routes.js
@@ -3,10 +3,11 @@ import { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import { Loader } from "components/Loader";
+import { NotFound } from "pages/errors";
 
-const About = lazy(() => import("./pages/about"));
-const Projects = lazy(() => import("./pages/projects"));
-const Contact = lazy(() => import("./pages/contact"));
+const About = lazy(() => import("pages/about"));
+const Projects = lazy(() => import("pages/projects"));
+const Contact = lazy(() => import("pages/contact"));
 
 export function Routes() {
   return (
@@ -15,6 +16,7 @@ export function Routes() {
         <Route exact path="/" component={About} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/contact" component={Contact} />
+        <Route component={NotFound} />
       </Switch>
     </Suspense>
   );
---
+++ src/pages/errors.js
@@ -0,0 +1,3 @@
+export function NotFound() {
+  return <h2>Page Not Found</h2>;
+}
