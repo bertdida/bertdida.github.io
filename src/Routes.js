500382ec2f52fd7f40892f0a6a3bbcf2d25274fc
bertdida <38489033+bertdida@users.noreply.github.com>
2020-12-29 18:57:40 +0800

    Use Loader as Suspense fallback

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import React, { lazy } from "react";
 import { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 
+import { Loader } from "components/Loader";
+
 const About = lazy(() => import("./pages/about"));
 const Projects = lazy(() => import("./pages/projects"));
 const Contact = lazy(() => import("./pages/contact"));
 
 export function Routes() {
   return (
-    <Suspense fallback="Lazy loading...">
+    <Suspense fallback={<Loader />}>
       <Switch>
         <Route exact path="/" component={About} />
         <Route exact path="/projects" component={Projects} />
