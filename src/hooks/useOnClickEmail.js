15a308a2654970593ed4f2658f22a55ece326ab7
bertdida <38489033+bertdida@users.noreply.github.com>
2021-06-12 10:45:32 +0800

    Fix double clicking email opens default mail provider (Fix #4)

--- src/hooks/useOnClickEmail.js
+++ src/hooks/useOnClickEmail.js
@@ -7,12 +7,13 @@ export function useOnClickEmail() {
   const { toasts, addToast } = useToast();
 
   function onClick(event) {
+    event.preventDefault();
+
     if (!navigator.clipboard) return;
     if (!event.currentTarget.hasAttribute("href")) return;
     if (!event.currentTarget.href.startsWith("mailto:")) return;
     if (toasts.length) return;
 
-    event.preventDefault();
     navigator.clipboard.writeText(event.currentTarget.href.substring(7));
 
     emojiIndex = emojiIndex >= emojis.length ? 0 : emojiIndex;
