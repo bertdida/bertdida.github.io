88174cb187f21d499a2856cb0fe7b61ece776b0d
Herbert Verdida <38489033+bertdida@users.noreply.github.com>
2022-03-22 21:12:06 +0800

    chore: update profile image and favicon

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 
 import { Link } from "components/Link";
-import profilePicture from "./assets/bertdida.jpeg";
+import profilePicture from "./assets/bertdida.jpg";
 import "./About.scss";
 
 export function About() {
