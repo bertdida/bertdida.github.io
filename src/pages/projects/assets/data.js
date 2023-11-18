6b4dbf195f16ffc50ce2b2487988e6b3d055ca82
Herbert Verdida <38489033+bertdida@users.noreply.github.com>
2023-11-18 21:41:16 +0800

    chore: updated project list

--- src/pages/projects/assets/data.js
+++ src/pages/projects/assets/data.js
@@ -1,7 +1,138 @@
 import { ExternalLink } from "../ExternalLink";
-import { DevJobsPhIcon, DeclareThatColorIcon, DevQuotesIcon } from "./icons";
+import {
+  DevJobsPhIcon,
+  DeclareThatColorIcon,
+  PurplerRoofIcon,
+  PoshSidekickIcon,
+  TheRisingNFTIcon,
+  LeadKlozerIcon,
+} from "./icons";
 
 export const projects = [
+  {
+    title: (
+      <ExternalLink to="https://purpleroof.com/">
+        <h3>Purple Roof</h3>
+      </ExternalLink>
+    ),
+    logo: <PurplerRoofIcon style={{ "--width": "35px" }} />,
+    description: (
+      <>
+        <p>
+          Purple Roof is a real estate firm specializing in property listing and
+          mortgage loan processing.
+        </p>
+        <p>
+          As the Lead Full-stack Developer, I was responsible for the complete
+          development of the application, from technical planning to final
+          deployment — key technologies employed include NextJS, MySQL,
+          Puppeteer, AWS S3, and Sendgrid.
+        </p>
+      </>
+    ),
+    tags: ["Next.js", "React", "Tailwind CSS", "AWS S3", "API Development"],
+    links: [],
+  },
+  {
+    title: (
+      <ExternalLink to="https://poshsidekick.com/">
+        <h3>Posh Sidekick</h3>
+      </ExternalLink>
+    ),
+    logo: <PoshSidekickIcon style={{ "--width": "33px" }} />,
+    description: (
+      <>
+        <p>
+          Posh Sidekick streamlines{" "}
+          <ExternalLink to="https://poshmark.com/">Poshmark</ExternalLink>{" "}
+          selling with automated sharing, following, and sending out offers,
+          ensuring continuous operation 24/7 without manual intervention.
+        </p>
+        <p>
+          Hired as a Mobile Developer and Automation Specialist, I developed an
+          <ExternalLink to="https://www.youtube.com/watch?v=ZccWRa4jLoQ">
+            automated live selling
+          </ExternalLink>{" "}
+          feature that was instrumental in increasing our customer base to over
+          500 in just a week after its launch.
+        </p>
+      </>
+    ),
+    tags: [
+      "React Native",
+      "Redux",
+      "Express.js",
+      "Puppeteer",
+      "Web Socket",
+      "Mobile Development",
+    ],
+    links: [
+      <ExternalLink to="https://play.google.com/store/apps/details?id=com.ppt.poshsidekick&hl=en&gl=US">
+        Google Play
+      </ExternalLink>,
+      <ExternalLink to="https://apps.apple.com/us/app/posh-sidekick/id1589949924">
+        App Store
+      </ExternalLink>,
+    ],
+  },
+  {
+    title: (
+      <ExternalLink to="therisingnft.com">
+        <h3>The Rising NFT</h3>
+      </ExternalLink>
+    ),
+    logo: <TheRisingNFTIcon />,
+    description: (
+      <>
+        <p>
+          Rising NFT is a collection of 1,120 unique digital artworks based on
+          &nbsp;
+          <ExternalLink to="https://www.justinhammerart.com/">
+            Justin Hammer
+          </ExternalLink>
+          's popular painting named "Rising".
+        </p>
+        <p>
+          This project marked my introduction to web3 development. In my role, I
+          handled a range of responsibilities such as deploying a smart contract
+          using Hardhat, integrating the MetaMask wallet, interacting with the
+          smart contract, and transforming wireframes into a React app.
+        </p>
+      </>
+    ),
+    tags: ["React", "Web3", "Metamask", "AWS CDK", "AWS Lambda"],
+    links: [],
+  },
+  {
+    title: (
+      <ExternalLink to="https://www.leadklozer.com/">
+        <h3>LeadKlozer</h3>
+      </ExternalLink>
+    ),
+    logo: <LeadKlozerIcon />,
+    description: (
+      <>
+        <p>
+          LeadKlozer is a social media management tool that helps businesses
+          track and engage with their leads on Facebook.
+        </p>
+        <p>
+          As a Full-stack Developer, I was in charge of adding key features to
+          the application. This included integrating Stripe for payments and
+          developing a feature that filters Facebook post engagement in real
+          time.
+        </p>
+      </>
+    ),
+    tags: [
+      "React",
+      "Laravel",
+      "Stripe",
+      "Facebook Graph API",
+      "API Development",
+    ],
+    links: [],
+  },
   {
     title: (
       <ExternalLink to="http://devjobsph.herokuapp.com">
@@ -22,13 +153,7 @@ export const projects = [
         </p>
       </>
     ),
-    tags: [
-      "MERN Stack",
-      "React Bootstrap",
-      "CRON",
-      "Web Scraping",
-      "API Development",
-    ],
+    tags: ["MERN Stack", "React", "CRON", "Web Scraping", "API Development"],
     links: [
       <ExternalLink to="https://github.com/bertdida/devjobsph">
         GitHub
@@ -58,7 +183,7 @@ export const projects = [
           <ExternalLink to="https://css-tricks.com/what-do-you-name-color-variables">
             What do you name color variables?
           </ExternalLink>
-          , and currently has 150+ installations on packagecontrol.io.
+          , and currently has 200+ installations on packagecontrol.io.
         </p>
         <p>
           I also built a web app version of this project, using Flask and React
@@ -77,48 +202,6 @@ export const projects = [
       </ExternalLink>,
       <ExternalLink to="https://github.com/bertdida/declare-that-color-web">
         GitHub - web
-      </ExternalLink>,
-    ],
-  },
-  {
-    title: (
-      <ExternalLink to="https://devquotes-react.vercel.app">
-        <h3>DevQuotes</h3>
-      </ExternalLink>
-    ),
-    logo: <DevQuotesIcon />,
-    description: (
-      <>
-        <p>
-          DevQuotes is a random quote generator built specifically for Software
-          Developers — it's a follow-up to my&nbsp;
-          <ExternalLink to="https://github.com/bertdida/twitter-quote-scraper">
-            TwitterQuoteScraper
-          </ExternalLink>
-          &nbsp;project.
-        </p>
-        <p>
-          This project allowed me to explore technologies such as Progressive
-          Web Apps, Firebase authentication, JSON Web Token, and Amazon
-          Elasticsearch Service.
-        </p>
-      </>
-    ),
-    tags: [
-      "Flask",
-      "PostgreSQL",
-      "React",
-      "Material-Ui",
-      "Firebase",
-      "PWA",
-      "API Development",
-    ],
-    links: [
-      <ExternalLink to="https://github.com/bertdida/devquotes-react">
-        GitHub - frontend
-      </ExternalLink>,
-      <ExternalLink to="https://github.com/bertdida/devquotes-flask">
-        GitHub - backend
       </ExternalLink>,
     ],
   },
--- src/pages/projects/assets/icons/index.js
+++ src/pages/projects/assets/icons/index.js
@@ -1,3 +1,16 @@
+import therisingnft from "./therisingnft.webp";
+import leadklozer from "./leadklozer.png";
+
 export { ReactComponent as DevJobsPhIcon } from "./devjobsph.svg";
 export { ReactComponent as DevQuotesIcon } from "./devquotes.svg";
 export { ReactComponent as DeclareThatColorIcon } from "./declarethatcolor.svg";
+export { ReactComponent as PurplerRoofIcon } from "./purpleroof.svg";
+export { ReactComponent as PoshSidekickIcon } from "./poshsidekick.svg";
+
+export const TheRisingNFTIcon = () => (
+  <img src={therisingnft} alt="The Rising NFT" style={{ width: "35px" }} />
+);
+
+export const LeadKlozerIcon = () => (
+  <img src={leadklozer} alt="LeadKlozer" style={{ width: "40px" }} />
+);
